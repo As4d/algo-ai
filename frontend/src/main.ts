86f61b13511d9fcce3c4af3dda1817597fbd168d
Asad <asadkhan91003@gmail.com>
2025-01-18 03:36:33 +0000

    Fix import statement for router in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css' // Using the default Vite CSS. Replace with your own global styles.
-import router from './router.ts'
+import router from './router'
 import App from './App.vue'
 import { useAuthStore } from './store/auth'
 
