8d33c12553ab6782c7d4dbed848f3b23e73d082a
Asad <asadkhan91003@gmail.com>
2025-02-22 16:07:44 +0000

    Add initial structure for Empty.vue component with placeholders for data, methods, and styles - following options API format

--- frontend/src/views/pages/Empty.vue
+++ frontend/src/views/pages/Empty.vue
@@ -4,3 +4,26 @@
         <p>Use this page to start from scratch and place your custom content.</p>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            // TODO: Define your reactive data properties here
+        };
+    },
+    mounted() {
+        // TODO: Add initialization logic here
+    },
+    beforeUnmount() {
+        // TODO: Clean up resources if needed
+    },
+    methods: {
+        // TODO: Add your methods here
+    }
+};
+</script>
+
+<style scoped>
+/* TODO: Add your CSS styles here */
+</style>
